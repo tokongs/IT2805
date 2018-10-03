4c0cc619c394772be24126594a1460f764a42c7f
Tobias Kongvik <tokongs98@gmail.com>
2018-10-03 09:27:28 +0200

    Small changes to assignment 5

--- assignment_5/script.js
+++ assignment_5/script.js
@@ -2,7 +2,7 @@
 /* Part 2 */
 console.log('PART 2');
 
-for (var i = 1; i < 21; i++) {
+for (let i = 1; i < 21; i++) {
     console.log(i);
 }
 
@@ -11,7 +11,7 @@ console.log('PART 3');
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-for (var i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 3 == 0) {
         console.log("eple")
     } else if (numbers[i] % 5 == 0) {
@@ -62,9 +62,9 @@ const technologies = [
 ];
 
 var tech = document.getElementById("tech");
-for(var i = 0; i < technologies.length; i++){
-    var li = document.createElement("LI");
-    var text = document.createTextNode(technologies[i]);
+for(let i = 0; i < technologies.length; i++){
+    let li = document.createElement("LI");
+    let text = document.createTextNode(technologies[i]);
     li.appendChild(text);
     tech.appendChild(li);
 }
